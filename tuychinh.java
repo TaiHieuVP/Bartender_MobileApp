7bcd6027999295c108a119600b4ea304f5d37f20
NEO-LeHuynhPhuoc <65266968+NEO-LeHuynhPhuoc@users.noreply.github.com>
2020-05-13 10:49:59 +0700

    Update tuychinh.java

--- tuychinh.java
+++ tuychinh.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class tuychinh extends AppCompatActivity {
+public class tuychinh extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
